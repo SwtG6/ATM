d9813c0c35ff0677a5d3636ecfba6cfd6429d45e
Jacob Svoldgaard <Jacob.svoldgaard93@gmail.com>
2019-10-30 23:31:52 +0100

    Complete deletion of TransponderReceiverClient. First iteration of the interface based on Franks from the PDF

--- AirTrafficMonitor/TransponderReceiver/ITransponderReceiverClient.cs
+++ AirTrafficMonitor/TransponderReceiver/ITransponderReceiverClient.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Calculator.TransponderReceiver;
 
 namespace Calculator.TransponderReceiver
 {
-    class ITransponderReceiverClient
+    public class RawTransponderDataEventArgs : EventArgs
     {
+        public RawTransponderDataEventArgs(List<string> transponderData)
+        {
+            TransponderData = transponderData;
+        }
+
+        public List<string> TransponderData { get; }
+    }
+
+    public interface ITransponderReceiver
+    {
+        event EventHandler<RawTransponderDataEventArgs> TransponderDataReady;
     }
 }
