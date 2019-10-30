fced567d3c7d2f8d8afd82bb37ad12e9d716cc78
JØ <201700290@post.au.dk>
2019-10-30 23:07:57 +0100

    Calculator class added

---
+++ AirTrafficMonitor/Calculator/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Calculator
+{
+    public static class Calculator
+    {
+
+        //Punkt 5
+        public static bool IsInsideAirSpace(int xpos, int ypos)
+        {
+            return (xpos >= 10000 && xpos <= 90000) && (ypos >= 10000 && ypos <= 90000);
+        }
+    }
+}
