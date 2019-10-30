54c8340fa09faba82b7899d17b6366c065fa77ac
Andreas Blaabjerg <au547352@uni.au.dk>
2019-10-30 19:08:46 +0100

    Loggers added with interfaces

---
+++ AirTrafficMonitor/ConsoleLogger/ConsoleLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.ConsoleLogger
+{
+    public class ConsoleLogger : IConsoleLogger
+    {
+        public void ClearLog()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogText(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
