f724ee083b9e6ca98ed3a31a3becf9700579d7dd
Jacob Svoldgaard <Jacob.svoldgaard93@gmail.com>
2019-10-30 17:29:46 +0100

    Change in "using" in Test.Unit

--- ATM.Test.Unit/Class1.cs
+++ ATM.Test.Unit/Class1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Calculator;
+using AirTrafficMonitor;
 using NUnit.Framework;
 
 namespace ATM.Test.Unit
