12c90ce3a924c24c5f6ddf96439a8c99903b170a
Jacob Svoldgaard <Jacob.svoldgaard93@gmail.com>
2019-10-30 15:24:24 +0100

    Add project files.

---
+++ ATM.Test.Unit/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator;
+using NUnit.Framework;
+
+namespace ATM.Test.Unit
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Add_TwoNumbers_ReturnEquals1()
+        {
+            // Arrange
+            var uut = new Calc();
+
+            // Act
+            uut.add(2, 3);
+
+            // Assert
+            Assert.That(uut.add(2, 3), Is.EqualTo(5));
+        }
+    }
+}
