c3782934cb490a0dd986e65303b66aefcd26d7b5
Jacob Svoldgaard <Jacob.svoldgaard93@gmail.com>
2019-10-30 16:13:00 +0100

    Further name changes

--- ATM.App/Program.cs
+++ ATM.App/Program.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Calculator;
+using AirTrafficMonitor;
 
-namespace CalculatorApp
+namespace ATMApp
 {
     class Program
     {
