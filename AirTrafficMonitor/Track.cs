73b56e0dc348c97ad20f61b945a99be7897b2e13
Andreas Blaabjerg <au547352@uni.au.dk>
2019-10-30 18:46:14 +0100

    Class Track added

---
+++ AirTrafficMonitor/Track.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Track
+    {
+        public string Tag { get; set; }
+        public int XCoordinate { get; set; }
+        public int YCoordinate { get; set; }
+        public int Altitude { get; set; }
+        public double HorizontalVelocity { get; set; }
+        public double CompassCourse { get; set; }
+        public DateTime Timer { get; set; }
+
+
+    }
+}
