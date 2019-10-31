673e27070116f4e659190014f1ba0c9691785f6c
Jacob Svoldgaard <Jacob.svoldgaard93@gmail.com>
2019-10-31 11:14:51 +0100

    First implementations in TransponderReceiver. Needs data handling/formatting before work can continue.

--- AirTrafficMonitor/TransponderReceiver/TransponderReceiverClient.cs
+++ AirTrafficMonitor/TransponderReceiver/TransponderReceiverClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,23 @@ namespace Calculator.TransponderReceiver
 
     public class TransponderReceiver : ITransponderReceiver
     {
+        private ITransponderReceiver DataReceiver;
 
+        public event EventHandler<RawTransponderDataEventArgs> TransponderDataReady;
+
+        public TransponderReceiver(ITransponderReceiver DataReceiver)
+        {
+            this.DataReceiver = DataReceiver;
+
+            this.DataReceiver.TransponderDataReady += HandleTransponderData;
+
+        }
+
+        public void HandleTransponderData(object o, RawTransponderDataEventArgs arg)
+        {
+
+
+        }
 
     }
 
