dd62b0581c808cc4e321e7460269a8f8cd0fcf31
Andreas Blaabjerg <au547352@uni.au.dk>
2019-10-31 12:17:49 +0100

    Comments added to ConditionLogger Test added to ConditionLoggerUnitTest - unsure if 100% coverage is reachable

--- ATM.Test.Unit/ConditionLoggerUnitTest.cs
+++ ATM.Test.Unit/ConditionLoggerUnitTest.cs
@@ -1,13 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+
 using System.Threading.Tasks;
+using Calculator.ConditionLogger;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 namespace ATM.Test.Unit
 {
+    [TestFixture]
     class ConditionLoggerUnitTest
     {
-        
+        private IConditionLogger uut;
+        private string Path;
+
+        [SetUp] // Setup making the uut + adding path for the testfolder location.
+        public void Setup()
+        {
+            uut = new ConditionLogger();
+            Path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        }
+
+        // Clear Log functionality will be necessary to reset the file that is tested on.
+        private void ClearLog()
+        {
+            File.WriteAllText(Path + @"\LogFile.txt", string.Empty);
+        }
+
+        [Test] // Test to see if we even have an existing log file.
+        public void TestMakeFile()
+        {
+            Assert.That(File.Exists(Path + @"\LogFile.txt"), Is.True);
+        }
+
+
     }
 }
--- AirTrafficMonitor/ConditionLogger/ConditionLogger.cs
+++ AirTrafficMonitor/ConditionLogger/ConditionLogger.cs
@@ -13,6 +13,9 @@ namespace Calculator.ConditionLogger
         private List<Tracks> TracksLogged;
         private string Path;
 
+
+        // ConditionLogger Constructor sets path to Local AppData and adds a .txt file called Log.
+        // if the file already exists it just instantiates the List of Tracks
         public ConditionLogger()
         {
             Path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Log.txt";
@@ -24,6 +27,7 @@ namespace Calculator.ConditionLogger
             TracksLogged = new List<Tracks>();
         }
 
+        // LogTracks function: Writes logtext for TrackTags and related Timers
         public void LogTracks(Tracks tracks)
         {
             if (!Logged(tracks))
@@ -33,6 +37,7 @@ namespace Calculator.ConditionLogger
             }
         }
 
+        // Bool function tool needed for if statement, to see if the Tracks are already logged.
         private bool Logged(Tracks tracks)
         {
             return TracksLogged.Exists(tl => (tl.New == tracks.New && tl.Old == tracks.Old) || (tl.New == tl.Old && tl.Old == tl.New));
