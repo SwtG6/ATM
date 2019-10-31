b7c4ad00ee8960f22f08f6661a5ba3302a1342b0
JØ <201700290@post.au.dk>
2019-10-31 14:18:08 +0100

    updated calculatro

--- AirTrafficMonitor/Calculator/Calculator.cs
+++ AirTrafficMonitor/Calculator/Calculator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Calculator.Track;
 
 namespace Calculator.Calculator
 {
@@ -10,7 +11,7 @@ namespace Calculator.Calculator
     {
 
         //Punkt 1&5 - Bruges til at validere om tracks er indenfor airspace
-        public static bool TrackIsInsideAirSpace(Track track)
+        public static bool TrackIsInsideAirSpace(Track.Track track)
         {
             if ((track.XCoordinate >= 10000 && track.XCoordinate <= 90000) &&
                 (track.YCoordinate >= 10000 && track.YCoordinate <= 90000) &&
@@ -25,7 +26,7 @@ namespace Calculator.Calculator
         }
 
         //Punkt 7 - current velocity
-        public static double GetCurrentVelocity(Track track1, Track track2)
+        public static double GetCurrentVelocity(Track.Track track1, Track.Track track2)
         {
             double time = (track2.Timer - track1.Timer).TotalMilliseconds;
 
@@ -33,7 +34,7 @@ namespace Calculator.Calculator
             return GetDistance(track1, track2)/time/1000;
         }
 
-        public static double GetDistance(Track track1, Track track2)
+        public static double GetDistance(Track.Track track1, Track.Track track2)
         {
             var deltaX = track2.XCoordinate - track1.XCoordinate;
             var deltaY = track2.YCoordinate - track1.YCoordinate;
@@ -47,7 +48,7 @@ namespace Calculator.Calculator
         }
 
         //Punkt 7 - current course
-        public static double GetCurrentCourse(Track track1, Track track2)
+        public static double GetCurrentCourse(Track.Track track1, Track.Track track2)
         {
             int deltaX = track2.XCoordinate - track1.XCoordinate;
             int deltaY = track2.YCoordinate - track1.YCoordinate;
@@ -67,7 +68,7 @@ namespace Calculator.Calculator
 
 
         //Punkt 12 - separation condition
-        public static bool AreTracksColliding(Track track1, Track track2)
+        public static bool AreTracksColliding(Track.Track track1, Track.Track track2)
         {
             if (GetDistance(track1, track2) < 5000 && 
                 GetAltitudeDistance(track1.Altitude, track2.Altitude) < 300 )
