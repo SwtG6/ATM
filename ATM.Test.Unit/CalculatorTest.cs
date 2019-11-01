11b830d9eca5190c1224f7db2c4fccfc355abb96
Jacob Svoldgaard <Jacob.svoldgaard93@gmail.com>
2019-11-01 21:53:07 +0100

    More CalculatorTest's added. And IDataFormat added to TransponderReceiverTest.

--- ATM.Test.Unit/CalculatorTest.cs
+++ ATM.Test.Unit/CalculatorTest.cs
@@ -19,14 +19,55 @@ namespace ATM.Test.Unit
         [Test]
         public void TrackInsideAirspaceTest()
         {
+            //List<Track> testTrack = new List<Track>();
+
             Track testTrack = new Track();
 
             testTrack.XCoordinate = 20000;
             testTrack.YCoordinate = 45000;
             testTrack.Altitude = 15000;
-            //result = true;
+            //result = true
 
             Assert.That(TrackIsInsideAirSpace(testTrack), Is.EqualTo(true));
+        }
+
+        [Test]
+        public void TrackOutsideXCoordinateLowAirspaceTest()
+        {
+            Track testTrack = new Track();
+
+            testTrack.XCoordinate = 2000;
+            testTrack.YCoordinate = 45000;
+            testTrack.Altitude = 15000;
+            //result = false
+
+            Assert.That(TrackIsInsideAirSpace(testTrack), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void TrackOutsideYCoordinateLowAirspaceTest()
+        {
+            Track testTrack = new Track();
+
+            testTrack.XCoordinate = 20000;
+            testTrack.YCoordinate = 4500;
+            testTrack.Altitude = 15000;
+            //result = false
+
+            Assert.That(TrackIsInsideAirSpace(testTrack), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void TrackOutsideXAltitudeHighAirspaceTest()
+        {
+            Track testTrack = new Track();
+
+            testTrack.XCoordinate = 2000;
+            testTrack.YCoordinate = 45000;
+            testTrack.Altitude = 150000;
+            //result = false
+
+            Assert.That(TrackIsInsideAirSpace(testTrack), Is.EqualTo(false));
         }
 
     }
