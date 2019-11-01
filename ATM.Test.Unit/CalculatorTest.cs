e14b3ed722fafa472082bc9cec3c11fe6a5ae8c5
Jacob Svoldgaard <Jacob.svoldgaard93@gmail.com>
2019-11-01 21:32:52 +0100

    Added Test: TrackInsideAirspaceTest to CalculatorTest

---
+++ ATM.Test.Unit/CalculatorTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficMonitor.Track;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TransponderReceiver;
+using Calculator = AirTrafficMonitor.Calculator.Calculator;
+using static AirTrafficMonitor.Calculator.Calculator;
+
+namespace ATM.Test.Unit
+{
+    [TestFixture]
+    class CalculatorTest
+    {
+
+        [Test]
+        public void TrackInsideAirspaceTest()
+        {
+            Track testTrack = new Track();
+
+            testTrack.XCoordinate = 20000;
+            testTrack.YCoordinate = 45000;
+            testTrack.Altitude = 15000;
+            //result = true;
+
+            Assert.That(TrackIsInsideAirSpace(testTrack), Is.EqualTo(true));
+        }
+
+    }
+}
