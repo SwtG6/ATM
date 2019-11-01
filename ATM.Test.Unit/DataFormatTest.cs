983c75541794b109a184dfb888fd8ef3418a706d
Jacob Svoldgaard <Jacob.svoldgaard93@gmail.com>
2019-11-01 16:19:57 +0100

    Complete re-iteration of the TransponderReceiver and DataFormat classes. TransponderReceiver has been completely backtracked to Franks, and the DataFormat-class has overtaken the jobs added here earlier.

--- ATM.Test.Unit/DataFormatTest.cs
+++ ATM.Test.Unit/DataFormatTest.cs
@@ -1,62 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AirTrafficMonitor.ConsoleRendering;
-using NSubstitute;
+﻿using NSubstitute;
 using NUnit.Framework;
 using TransponderReceiver;
 using AirTrafficMonitor.Data;
-using AirTrafficMonitor.Track;
-using AirTrafficMonitor.TransponderReceiver;
-using AirTrafficMonitor.TrackHandler;
-using Track = AirTrafficMonitor.Track.Track;
+using TransponderDataEventArgs = TransponderReceiver.TransponderDataEventArgs;
 
 namespace ATM.Test.Unit
 {
     [TestFixture]
     class DataFormatTest
     {
-        //DataFormat uut;
-
-        //private IDataFormat _fakeDataFormat;
-        private DataFormat  _uut;
-
+        private ITransponderReceiver _fakeTransponderReceiver { get; set; }
+        private TransponderDataEventArgs dataFormatEventArgs { get; set; }
+        private IDataFormat UUT { get; set; }
 
         [SetUp]
         public void setup()
         {
-            _uut = new DataFormat();
+            _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
 
-            //_fakeDataFormat = Substitute.For<IDataFormat>();
-
-            //_uut = new DataFormat(_fakeDataFormat);
-
+            UUT = new DataFormat(_fakeTransponderReceiver);
         }
 
-        // Setup test data
-
-        [TestCase("ATR423;39045;12932;14000;20151006213456789")]
-        public void TrackTagDataFormatTest(string testString)
+        [Test]
+        public void FlightDataFormattedTest()
         {
+            var testTrackData = UUT.CreateTrack("ATR423;39045;12932;14000;20151006213456789");
 
-            Track track = _uut.CreateTrack(testString);
-
-            Assert.That(track.Tag, Is.EqualTo("ATR423"));
-
+            Assert.That(testTrackData.Tag, Is.EqualTo("ATR423"));
         }
-
-
-        //[Test]
-        //public void TestReception()
-        //{
-        //List<string> testData = new List<string>();
-        //testData.Add("ATR423;39045;12932;14000;20151006213456789");
-
-        //_fakeDataFormat.TransponderDataReady
-        //    += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
-
     }
+
 }
+
+
+
+
+//    [TestFixture]
+//    class DataFormatTest
+//    {
+//        //DataFormat uut;
+
+//        //private IDataFormat _fakeDataFormat;
+//        //private DataFormat  _uut;
+
+//        private ITransponderReceiver _fakeTransponderReceiver;
+//        //private IDataFormat _fakeDataFormat;
+
+
+//        [SetUp]
+//        public void setup()
+//        {
+//            _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
+
+//            _uut = new TransponderReceiverClient(_fakeTransponderReceiver);
+
+//            //_fakeDataFormat = Substitute.For<IDataFormat>();
+
+//            //_uut = new DataFormat(_fakeDataFormat);
+
+//        }
+
+//        // Setup test data
+
+//        [TestCase("ATR423;39045;12932;14000;20151006213456789")]
+//        public void TrackTagDataFormatTest(string testString)
+//        {
+
+//            Track track = _uut.CreateTrack(testString);
+
+//            Assert.That(track.Tag, Is.EqualTo("ATR423"));
+
+//        }
+
+
+//        //[Test]
+//        //public void TestReception()
+//        //{
+//        //List<string> testData = new List<string>();
+//        //testData.Add("ATR423;39045;12932;14000;20151006213456789");
+
+//        //_fakeDataFormat.TransponderDataReady
+//        //    += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
+
+//    }
+//}
 
--- ATM.Test.Unit/TransponderReceiverTest.cs
+++ ATM.Test.Unit/TransponderReceiverTest.cs
@@ -8,52 +8,26 @@ using AirTrafficMonitor.TransponderReceiver;
 using NSubstitute;
 using NUnit.Framework;
 using TransponderReceiver;
+using AirTrafficMonitor.Track;
+
 
 namespace ATM.Test.Unit
 {
-    [TestFixture]
-    public class TransponderReceiverTest
+
+    public class TestTransponderReceiverClient
     {
         private ITransponderReceiver _fakeTransponderReceiver;
-        private IDataFormat _fakeDataFormat;
-
         private TransponderReceiverClient _uut;
-
-        //private int _numberOfEvents;
 
         [SetUp]
         public void Setup()
         {
-
-            //_numberOfEvents = 0;
-
-
             // Make a fake Transponder Data Receiver
             _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
-            _fakeDataFormat = Substitute.For<IDataFormat>();
-
             // Inject the fake TDR
-            _uut = new TransponderReceiverClient(_fakeTransponderReceiver, _fakeDataFormat);
-
-
-            //_uut.TransponderDataReady += (sender, args) => _numberOfEvents++;
+            _uut = new TransponderReceiverClient(_fakeTransponderReceiver);
         }
 
-
-        //[TestCase("ATR423;39045;12932;14000;20151006213456789")]
-        //[TestCase("BCD123;10005;85890;12000;20151006213456789")]
-        //[TestCase("XYZ987;25059;75654;4000;20151006213456789")]
-        //public void ReceiverOnTransponderDataReady_CreateEvent_DataformatCreateTrackCalled(string input)
-        //{
-        //    List<string> testData = new List<string>();
-        //    testData.Add(input);
-
-        //    //_fakeTransponderReceiver.TransponderDataReady
-        //    //    += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
-
-        //    _fakeDataFormat.Received(1).CreateTrack(input);
-
-        //}
         [Test]
         public void TestReception()
         {
@@ -64,18 +38,132 @@ namespace ATM.Test.Unit
             testData.Add("XYZ987;25059;75654;4000;20151006213456789");
 
             // Act: Trigger the fake object to execute event invocation
-            _fakeTransponderReceiver.TransponderDataReady += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
 
             // Assert something here or use an NSubstitute Received
-
-
-
-            //Assert.AreEqual(1, _numberOfEvents);
-
-
-            //_fakeDataFormat.Received(1).CreateTrack(testData);
-
         }
-
     }
 }
+
+
+
+
+
+
+//[TestFixture]
+    //public class TransponderReceiverTest
+    //{
+
+
+
+
+
+
+    //private ITransponderReceiver _fakeTransponderReceiver;
+    //private IDataFormat _fakeDataFormat;
+
+    //private TransponderReceiverClient _uut;
+
+    //private int _numberOfEvents;
+
+    //[SetUp]
+    //public void Setup()
+    //{
+    //    _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
+    //    //    _fakeDataFormat = Substitute.For<IDataFormat>();
+
+    //    _uut = new DataFormat(_fakeTransponderReceiver);
+
+
+    //}
+
+
+    //[Test]
+    //public void DataFormatTest()
+    //{
+    //    // Setup test data
+
+    //    var testTrack = _uut.CreateTrack("ATR423;39045;12932;14000;20151006213456789");
+
+    //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //    _numberOfEvents = 0;
+
+
+    //    // Make a fake Transponder Data Receiver
+    //    _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
+    //    _fakeDataFormat = Substitute.For<IDataFormat>();
+
+    //    // Inject the fake TDR
+    //    _uut = new TransponderReceiverClient(_fakeTransponderReceiver, _fakeDataFormat);
+
+
+    //    _uut.TransponderDataReady += (sender, args) => _numberOfEvents++;
+    //}
+
+
+    //[TestCase("ATR423;39045;12932;14000;20151006213456789")]
+    ////[TestCase("BCD123;10005;85890;12000;20151006213456789")]
+    ////[TestCase("XYZ987;25059;75654;4000;20151006213456789")]
+    //public void ReceiverOnTransponderDataReady_CreateEvent_DataFormatCreateTrackCalled(string input)
+    //{
+    //    List<string> testData = new List<string>();
+    //    testData.Add(input);
+
+    //    _fakeTransponderReceiver.TransponderDataReady
+    //        += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
+
+    //    //_fakeDataFormat.Received(1).CreateTrack(input);
+
+
+
+    //    Assert.AreEqual(1, _numberOfEvents);
+    //}
+
+
+
+
+
+
+
+
+
+
+    //    List<string> testData = new List<string>();
+    //    testData.Add("ATR423;39045;12932;14000;20151006213456789");
+    //    testData.Add("BCD123;10005;85890;12000;20151006213456789");
+    //    testData.Add("XYZ987;25059;75654;4000;20151006213456789");
+
+    //    // Act: Trigger the fake object to execute event invocation
+    //    _fakeTransponderReceiver.TransponderDataReady += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
+
+    // Assert something here or use an NSubstitute Received
+
+
+
+
+
+
+    //_fakeDataFormat.Received(1).CreateTrack(testData);
+
+
+
