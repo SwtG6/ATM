1a55fa2208eb74420542b260712202bbc191370c
Jacob Svoldgaard <Jacob.svoldgaard93@gmail.com>
2019-11-19 16:30:45 +0100

    GetDistance and GetAltitudeDistance Tests. Plus collision tests.

--- ATM.Test.Unit/CalculatorTest.cs
+++ ATM.Test.Unit/CalculatorTest.cs
@@ -239,20 +239,20 @@ namespace ATM.Test.Unit
             Assert.That(GetDistance(testTrackDist1, testTrackDist2), Is.EqualTo(500));
         }
 
-        [Test] // Test 15: Test om GetDistance-funktion virker efter hensigten ved forskel i X- og Y-koordinat
+        [Test] // Test 15: Test om GetDistance-funktion virker efter hensigten ved forskel i både X- og Y-koordinat
         public void DistanceBetweenTracksXYTest()
         {
             Track testTrackDist1 = new Track();
-            testTrackDist1.XCoordinate = 90000;
-            testTrackDist1.YCoordinate = 90000;
+            testTrackDist1.XCoordinate = 45000;
+            testTrackDist1.YCoordinate = 55000;
             testTrackDist1.Altitude = 10000;
 
             Track testTrackDist2 = new Track();
-            testTrackDist2.XCoordinate = 10000;
-            testTrackDist2.YCoordinate = 10000;
+            testTrackDist2.XCoordinate = 55000;
+            testTrackDist2.YCoordinate = 45000;
             testTrackDist2.Altitude = 10000;
 
-            Assert.That(GetDistance(testTrackDist1, testTrackDist2), Is.EqualTo(100000));
+            Assert.That(GetDistance(testTrackDist1, testTrackDist2), Is.EqualTo(14140));
         }
 
         [Test] // Test 16: Test om GetDistance-funktion virker efter hensigten ved ingen forskel i koordinator
@@ -274,9 +274,67 @@ namespace ATM.Test.Unit
 
         #endregion GetDistance Test
 
+        [Test] // Test 17: Udregning af altitude afstanden mellem to placeringer.
+        public void AltitudeDistanceTest()
+        {
+            Track testAltTrack1 = new Track();
+            testAltTrack1.Altitude = 10000;
+
+            Track testAltTrack2 = new Track();
+            testAltTrack2.Altitude = 15000;
+
+            Assert.That(GetAltitudeDistance(testAltTrack1.Altitude, testAltTrack2.Altitude), Is.EqualTo(5000));
+        }
+
+        [Test] // Test 18: Udregning af forskellen i kurs mellem to tracks med forskellige X- og Y-koordinator.
+        public void CurrentCourseTest()
+        {
+            Track testCourseTrack1 = new Track();
+            testCourseTrack1.XCoordinate = 20000;
+            testCourseTrack1.YCoordinate = 40000;
+
+            Track testCourseTrack2 = new Track();
+            testCourseTrack2.XCoordinate = 30000;
+            testCourseTrack2.YCoordinate = 50000;
+
+            Assert.That(GetCurrentCourse(testCourseTrack1, testCourseTrack2), Is.EqualTo(45));
+        }
+
         #region Collision Test
 
-        [Test] // Test 6: Tester om 2 fly er ved at kollidere, baseret på vores GetDistance-funktion.
+        [Test] // Test 19: Tester om 2 fly er ved at kollidere, baseret på vores GetDistance- og GetAltitudeDistance-funktioner. Hvor altitude forskellen gør at de ikke bør kollidere.
+        public void TracksCollidingTestOnlyXY()
+        {
+            Track testTrack1 = new Track();
+            testTrack1.XCoordinate = 42000;
+            testTrack1.YCoordinate = 45000;
+            testTrack1.Altitude = 10000;
+
+            Track testTrack2 = new Track();
+            testTrack2.XCoordinate = 45000;
+            testTrack2.YCoordinate = 45000;
+            testTrack2.Altitude = 9000;
+
+            Assert.That(AreTracksColliding(testTrack1, testTrack2), Is.EqualTo(false));
+        }
+
+        [Test] // Test 20: Tester om 2 fly er ved at kollidere, baseret på vores GetDistance- og GetAltitudeDistance-funktioner. Hvor X/Y-forskellen gør at de ikke bør kollidere.
+        public void TracksCollidingTestOnly()
+        {
+            Track testTrack1 = new Track();
+            testTrack1.XCoordinate = 40000;
+            testTrack1.YCoordinate = 45000;
+            testTrack1.Altitude = 10000;
+
+            Track testTrack2 = new Track();
+            testTrack2.XCoordinate = 45000;
+            testTrack2.YCoordinate = 45000;
+            testTrack2.Altitude = 9900;
+
+            Assert.That(AreTracksColliding(testTrack1, testTrack2), Is.EqualTo(false));
+        }
+
+        [Test] // Test 21: Tester om 2 fly er ved at kollidere, baseret på vores GetDistance- og GetAltitudeDistance-funktioner.
         public void TracksCollidingTest()
         {
             Track testTrack1 = new Track();
@@ -296,31 +354,9 @@ namespace ATM.Test.Unit
 
 
 
-        [Test] // Test 7: Udregning af altitude afstanden mellem to placeringer.
-        public void AltitudeDistanceTest()
-        {
-            Track testAltTrack1 = new Track();
-            testAltTrack1.Altitude = 10000;
 
-            Track testAltTrack2 = new Track();
-            testAltTrack2.Altitude = 15000;
 
-            Assert.That(GetAltitudeDistance(testAltTrack1.Altitude, testAltTrack2.Altitude), Is.EqualTo(5000));
-        }
 
-        [Test] // Test 8: Udregning af forskellen i kurs mellem to placeringer.
-        public void CurrentCourseTest()
-        {
-            Track testCourseTrack1 = new Track();
-            testCourseTrack1.XCoordinate = 20000;
-            testCourseTrack1.YCoordinate = 40000;
-
-            Track testCourseTrack2 = new Track();
-            testCourseTrack2.XCoordinate = 30000;
-            testCourseTrack2.YCoordinate = 50000;
-
-            Assert.That(GetCurrentCourse(testCourseTrack1, testCourseTrack2), Is.EqualTo(45));
-        }
 
 
         //[Test]
