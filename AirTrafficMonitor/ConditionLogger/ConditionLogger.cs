f286f5ce38e4fd356a85baf8b1b6501b6f3a67cf
Bl0bb3r <34944047+Bl0bb3r@users.noreply.github.com>
2019-11-19 15:38:22 +0100

    Folder path changed in ConditionLogger

--- AirTrafficMonitor/ConditionLogger/ConditionLogger.cs
+++ AirTrafficMonitor/ConditionLogger/ConditionLogger.cs
@@ -10,22 +10,24 @@ namespace AirTrafficMonitor.ConditionLogger
 {
     public class ConditionLogger : IConditionLogger
     {
-        private List<Tracks> TracksLogged;
+        private readonly List<Tracks> _tracksLogged;
         private string Path;
+        private string LogFile;
 
 
         // ConditionLogger Constructor sets path to Local AppData and adds a .txt file called Log.
         // if the file already exists it just instantiates the List of Tracks
         public ConditionLogger()
         {
-            // https://stackoverflow.com/questions/2501167/what-directories-do-the-different-application-specialfolders-point-to-in-windows
-            Path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Log.txt";
+            // removed AppData usage changed to documents. now saved as Log.txt in path: "C:\\Users\\FlemmingBlaabjerg\\Documents\\Log.txt"
+            LogFile = @"\Log.txt";
+            Path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + LogFile;
             if (!File.Exists(Path))
             {
-                var filePath = File.Create(Path);
-                filePath.Close();
+                var fileStream = File.Create(Path);
+                fileStream.Close();
             }
-            TracksLogged = new List<Tracks>();
+            _tracksLogged = new List<Tracks>();
         }
 
         // LogTracks function: Writes logtext for TrackTags and related Timers
@@ -34,14 +36,14 @@ namespace AirTrafficMonitor.ConditionLogger
             if (!Logged(tracks))
             {
                 File.AppendAllText(Path, $"{tracks.New.Tag};{tracks.Old.Tag};{tracks.New.Timer.ToString()}" + Environment.NewLine);
-                TracksLogged.Add(tracks);
+                _tracksLogged.Add(tracks);
             }
         }
 
         // Bool function tool needed for if statement, to see if the Tracks are already logged.
         private bool Logged(Tracks tracks)
         {
-            return TracksLogged.Exists(tl => (tl.New == tracks.New && tl.Old == tracks.Old) || (tl.New == tl.Old && tl.Old == tl.New));
+            return _tracksLogged.Exists(tl => (tl.New == tracks.New && tl.Old == tracks.Old) || (tl.New == tl.Old && tl.Old == tl.New));
         }
     }
 }
