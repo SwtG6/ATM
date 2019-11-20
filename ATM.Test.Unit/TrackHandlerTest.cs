e10d357d2296999181ff26e01577f15e748035c2
Jacob Svoldgaard <Jacob.svoldgaard93@gmail.com>
2019-11-20 14:19:29 +0100

    Udkommentering af støre dele af TrackHandlerTest i forsøg på at finde en fejl.

--- ATM.Test.Unit/TrackHandlerTest.cs
+++ ATM.Test.Unit/TrackHandlerTest.cs
@@ -72,36 +72,36 @@ namespace ATM.Test.Unit
         }
 
 
-        [SetUp] // Adding a track
+       // [SetUp] // Adding a track
 
-       // https://stackoverflow.com/questions/7068843/how-to-use-datetime-parse-to-create-a-datetime-object/7068890
-        public DateTime Parse(string time)
-        {
-            return DateTime.ParseExact(time, "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture);
-        }
+       //// https://stackoverflow.com/questions/7068843/how-to-use-datetime-parse-to-create-a-datetime-object/7068890
+       // public DateTime Parse(string time)
+       // {
+       //     return DateTime.ParseExact(time, "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture);
+       // }
 
-        public List<Track> AddTracks()
-        {
-            List<Track> t1 = new List<Track>();
-            t1.Add(new Track
-            {
-                Altitude = 10000,
-                Tag = "ABC1337",
-                Timer = Parse("20191205121106300"),
-                XCoordinate = 6666,
-                YCoordinate = 6969
-            });
-            t1.Add(new Track
-            {
-                Altitude = 12000,
-                Tag = "DEF1234",
-                Timer = Parse("20191205121107400"),
-                XCoordinate = 20000,
-                YCoordinate = 10000
-            });
+        //public List<Track> AddTracks()
+        //{
+        //    List<Track> t1 = new List<Track>();
+        //    t1.Add(new Track
+        //    {
+        //        Altitude = 10000,
+        //        Tag = "ABC1337",
+        //        Timer = Parse("20191205121106300"),
+        //        XCoordinate = 6666,
+        //        YCoordinate = 6969
+        //    });
+        //    t1.Add(new Track
+        //    {
+        //        Altitude = 12000,
+        //        Tag = "DEF1234",
+        //        Timer = Parse("20191205121107400"),
+        //        XCoordinate = 20000,
+        //        YCoordinate = 10000
+        //    });
 
-            return t1;
-        }
+        //    return t1;
+        //}
 
         //[Test] // Adding a track
 
