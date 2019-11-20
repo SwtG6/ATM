2a761b83eef814889ab947541e44f5eba999a8f1
Jacob Svoldgaard <Jacob.svoldgaard93@gmail.com>
2019-11-20 12:50:31 +0100

    Udkommenterede hele TrackHandlerTest for at tjekke coverage på andre dele af projektet

--- ATM.Test.Unit/TrackHandlerTest.cs
+++ ATM.Test.Unit/TrackHandlerTest.cs
@@ -15,92 +15,92 @@ using TransponderReceiver;
 namespace ATM.Test.Unit
 {
 
-    [TestFixture]
-    class TrackHandlerTest
-    {
-        private List<Track> tracks1 = new List<Track>();
-        private List<Track> tracks2 = new List<Track>();
+//    [TestFixture]
+//    class TrackHandlerTest
+//    {
+//        private List<Track> tracks1 = new List<Track>();
+//        private List<Track> tracks2 = new List<Track>();
 
-        private List<Tracks> collisionTracks = new List<Tracks>();
+//        private List<Tracks> collisionTracks = new List<Tracks>();
 
-        private TrackHandler uut;
-        private IDataFormat tr_interface;
-        private IConditionLogger logger;
+//        private TrackHandler uut;
+//        private IDataFormat tr_interface;
+//        private IConditionLogger logger;
 
-        [SetUp]
-        public void Setup()
-        {
-            tr_interface = Substitute.For<IDataFormat>();
-            logger = new ConditionLogger();
-            uut.RaiseEvent += RaiseEventHandler;
+//        [SetUp]
+//        public void Setup()
+//        {
+//            tr_interface = Substitute.For<IDataFormat>();
+//            logger = new ConditionLogger();
+//            uut.RaiseEvent += RaiseEventHandler;
 
-        }
+//        }
 
-        private void RaiseEventHandler(object o, TrackUpdateEvent e)
-        {
-            tracks1 = e.ListOfNewTracks;
-            tracks2 = e.ListOfUpdatedTracks;
-            collisionTracks = e.ListOfCollidingTracks;
+//        private void RaiseEventHandler(object o, TrackUpdateEvent e)
+//        {
+//            tracks1 = e.ListOfNewTracks;
+//            tracks2 = e.ListOfUpdatedTracks;
+//            collisionTracks = e.ListOfCollidingTracks;
 
-        }
+//        }
 
-        [Test] // Tag
-        public void TagTest()
-        {
-            Track tx = new Track
-            {
-                Altitude = 30000,
-                Tag = "ABC1337"
-            };
+//        [Test] // Tag
+//        public void TagTest()
+//        {
+//            Track tx = new Track
+//            {
+//                Altitude = 30000,
+//                Tag = "ABC1337"
+//            };
 
-            Track ty = new Track
-            {
-                Altitude = 12500,
-                Tag = "ABC1337"
-            };
+//            Track ty = new Track
+//            {
+//                Altitude = 12500,
+//                Tag = "ABC1337"
+//            };
 
-            Assert.That(tx == ty,Is.True);
+//            Assert.That(tx == ty,Is.True);
 
-            ty.Tag = "XYZ742";
+//            ty.Tag = "XYZ742";
 
-            Assert.That(tx != ty, Is.True);
+//            Assert.That(tx != ty, Is.True);
 
-        }
-
-
-        [SetUp] // Adding a track
-
-        // https://stackoverflow.com/questions/7068843/how-to-use-datetime-parse-to-create-a-datetime-object/7068890
-        private DateTime Parse(string time)
-        {
-            return DateTime.ParseExact(time, "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture);
-        }
-
-        public List<Track> AddTracks()
-        {
-            List<Track> t1 = new List<Track>();
-            t1.Add(new Track
-            {
-                Altitude = 10000,
-                Tag = "ABC1337",
-                Timer = Parse("20191205121106300"),
-                XCoordinate = 6666,
-                YCoordinate = 6969
-            });
-
-            return t1;
-        }
-
-/*        [Test]*/ // Adding a track
-
-        //public void TestAddTracks()
-        //{
-        //    List<Track> t1 = AddTracks();
-        //    tr_interface.TransponderDataReady += Raise.Event();
-
-        //}
+//        }
 
 
+//        [SetUp] // Adding a track
 
-    }
+//        // https://stackoverflow.com/questions/7068843/how-to-use-datetime-parse-to-create-a-datetime-object/7068890
+//        private DateTime Parse(string time)
+//        {
+//            return DateTime.ParseExact(time, "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture);
+//        }
+
+//        public List<Track> AddTracks()
+//        {
+//            List<Track> t1 = new List<Track>();
+//            t1.Add(new Track
+//            {
+//                Altitude = 10000,
+//                Tag = "ABC1337",
+//                Timer = Parse("20191205121106300"),
+//                XCoordinate = 6666,
+//                YCoordinate = 6969
+//            });
+
+//            return t1;
+//        }
+
+///*        [Test]*/ // Adding a track
+
+//        //public void TestAddTracks()
+//        //{
+//        //    List<Track> t1 = AddTracks();
+//        //    tr_interface.TransponderDataReady += Raise.Event();
+
+//        //}
+
+
+
+//    }
 }
